6452199c6f131c1f3524387ab9efc8a23bc21dcc
Javier <contact@javierquiroga.com>
2026-08-18 14:33:05 +0100

    Initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,7 @@
+import type { RouteObject } from 'react-router-dom';
+
+import Root from './routes/Root';
+
+const routes: RouteObject[] = [{ path: '/', element: <Root /> }];
+
+export default routes;
