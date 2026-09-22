c06e793e1f7e9cda836cdea8c79ed9a514e1087f
Javier Quiroga <contact@javierquiroga.com>
2026-09-22 15:17:31 +0100

    feat: wire up self-hosted fonts via Fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import routes from './routes';
 import './styles/tokens.css';
+import './styles/global.css';
 
 const router = createBrowserRouter(routes);
 
